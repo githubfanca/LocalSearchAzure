67e5de27e43dab3c9625eb13c3989069545e760e
liulike <liulike.cn@gmail.com>
2014-05-15 23:25:34 -0700

    Added information class

---
+++ LocalEntitySearch/Information.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace LocalEntitySearch
+{
+    [DataContract]
+    public class Information
+    {
+        [DataMember]
+        public string ServerId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string TableId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Updated
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
