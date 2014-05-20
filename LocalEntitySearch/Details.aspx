c9e467c98ccef77632912d61faaa282e1eb5a0b8
liulike <liulike.cn@gmail.com>
2014-05-19 20:57:18 -0700

    Add details page

---
+++ LocalEntitySearch/Details.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Details.aspx.cs" Inherits="LocalEntitySearch.Local" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <iframe name="myIframe" id="myIframe" width="1924px" height="668px" runat =server src=""></iframe>
+        
+    </div>
+        <asp:Image ID="QRcode" runat="server" Margin="20,168,0,0"/>
+    </form>
+</body>
+</html>
---
+++ LocalEntitySearch/Details.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LocalEntitySearch
+{
+    public partial class Local : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var id = Request.QueryString["lid"];
+            if(string.IsNullOrWhiteSpace(id))
+            {
+                id = "YN873x2752735941963594177";
+            }
+            var url = string.Format("http://www.bing.com/local?lid={0}", id);
+
+            myIframe.Src = url;
+
+            QRcode.ImageUrl = string.Format("http://www.esponce.com/api/v3/generate?content={0}&format=png", HttpUtility.UrlEncode(url));
+        }
+    }
+}
